b24c4e47e20238dd1c7c5ae117ecd60d72c1a4ab
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 22:22:48 +0600

    problem 5

--- final.js
+++ final.js
@@ -40,3 +40,11 @@ if (
 } else {
   console.log("Delete");
 }
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+//write your code here
+console.log(
+  (email = student.name + student.roll + "." + student.department + "@ph.ac.bd")
+);
